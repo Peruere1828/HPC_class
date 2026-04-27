0f506d8c544c0c3db07bab525e9c6b35f2aee35d
Peruere1828 <fmyh1828@gmail.com>
2026-04-27 03:05:51 +0000

    Task3: add report

--- task3/README.md
+++ task3/README.md
@@ -1,10 +1,32 @@
 利用 `MPI_Send` 和 `MPI_Recv` 实现 `MPI_Bcast`。
 
-采用树形通信
+采用树形通信实现。
 
 运行：
 
 ```
 make
 mpirun -n 6 ./main
+```
+
+输出结果：
+
+```
+root=2 casting=12345
+myrank=2 send to=3
+myrank=2 send to=4
+myrank=1 recv from=4
+myrank=3 recv from=2
+myrank=3 send to=5
+myrank=3 send to=0
+myrank=4 recv from=2
+myrank=4 send to=1
+myrank=5 recv from=3
+myrank=0 recv from=3
+myrank=0 receive=12345
+myrank=5 receive=12345
+myrank=2 receive=12345
+myrank=1 receive=12345
+myrank=3 receive=12345
+myrank=4 receive=12345
 ```
--- task3/main.cpp
+++ task3/main.cpp
@@ -10,6 +10,7 @@ void my_MPI_Bcast(void *data, int count, MPI_Datatype datatype, int root) {
     int parent = ((rel_rank - 1) / 2 + root) % cpusize;
     MPI_Recv(data, count, datatype, parent, 0, MPI_COMM_WORLD,
              MPI_STATUS_IGNORE);
+    printf("myrank=%d recv from=%d\n", myrank, parent);
   }
 
   int rel_child1 = 2 * rel_rank + 1;
@@ -18,10 +19,12 @@ void my_MPI_Bcast(void *data, int count, MPI_Datatype datatype, int root) {
   if (rel_child1 < cpusize) {
     int child1 = (rel_child1 + root) % cpusize;
     MPI_Send(data, count, datatype, child1, 0, MPI_COMM_WORLD);
+    printf("myrank=%d send to=%d\n", myrank, child1);
   }
   if (rel_child2 < cpusize) {
     int child2 = (rel_child2 + root) % cpusize;
     MPI_Send(data, count, datatype, child2, 0, MPI_COMM_WORLD);
+    printf("myrank=%d send to=%d\n", myrank, child2);
   }
 }
 
@@ -39,6 +42,8 @@ int main(int argc, char *argv[]) {
   }
 
   my_MPI_Bcast(&value, 1, MPI_INT, root);
+
+  MPI_Barrier(MPI_COMM_WORLD);
 
   printf("myrank=%d receive=%d\n", myrank, value);
   MPI_Finalize();
